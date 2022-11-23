e0d23051cb5404a6240c7a12816f4ad596af3847
YooJaeHyeon <68884608+YooJaeHyeon@users.noreply.github.com>
2022-11-23 16:15:17 +0900

    initial setting

---
+++ src/main/java/com/jhyoo/opggclone/opggclone/OpggcloneApplication.java
@@ -0,0 +1,13 @@
+package com.jhyoo.opggclone.opggclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpggcloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpggcloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhyoo/opggclone/opggclone/OpggcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhyoo.opggclone.opggclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpggcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
