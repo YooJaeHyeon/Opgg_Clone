2889deeb59a8dad12e05f7e865bb02539362edc8
YooJaeHyeon <jhyoo1203@gmail.com>
2022-11-24 09:51:44 +0900

    feat: 메인 페이지 디자인 수정

--- src/main/frontend/src/components/Mainpage.js
+++ src/main/frontend/src/components/Mainpage.js
@@ -16,9 +16,18 @@ function Mainpage() {
             <div>
                 백엔드에서 가져온 데이터입니다 : {hello}
             </div>
+            <div className="opgg_logo">
+                OP.GG
+            </div>
             <div className="search">
-                <input type="text" placeholder='소환사명' id='search' />
-                <button type="submit">.GG</button>
+                <form onSubmit={event=>{
+                    event.preventDefault();
+                    const summoner = event.target.summoner.value;
+                    console.log(summoner);
+                }}>
+                    <input type="text" name="summoner" placeholder='소환사명' />
+                    <button type="submit">.GG</button>
+                </form>
             </div>
         </>
     )
